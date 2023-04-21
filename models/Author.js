19c596c6832a4bb3b9cd8db79f99e30522db4460
CATorres Varon <44732360+ctorresv@users.noreply.github.com>
2023-04-21 13:53:12 -0500

    b-03 (#3)

---
+++ models/Author.js
@@ -0,0 +1,23 @@
+import mongoose, {Schema, model,Types} from "mongoose"
+
+let shema = new Schema({
+    name: { type: String, required: true },
+    last_name: { type: String, required: false },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+    date: { type:Date },
+    photo: { type: String, required: true },
+    active: { type: Boolean, required: true },
+    user_id: { 
+        type: Types.ObjectId, //Tipos de datos de moongose me permite relacionar datos de una coleccion con datos de otra coleccion
+        ref: 'users', // nombre de la coleccion con la que se quiere referenciar
+        required: true
+    } 
+},{
+    timestamps: true
+})
+
+let collection = 'authors'
+
+let Author = model(shema,collection)
+export default Author
